3eb9ed1147cb5117c73ab32cb3c48d5823c96d1b
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-01 21:09:01 -0700

    working on convert function in bases.py

--- Lessons/source/bases.py
+++ Lessons/source/bases.py
@@ -57,11 +57,9 @@ def encode(number, base):
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
 
-    result = 0
+    result = ''
 
-    # an array for popping the least significant bit.
-    # digits_array = list(number)
-
+    lookup = { '0':'0',  '1':'1',  '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9', '10':'A', '11':'B', '12': 'C', '13':'D', '14':'E', '15':'F', '10':'a', '11':'b', '12':'c', '13':'d', '14':'e', '15':'f'}
 
     # TODO: Encode digits to binary (base 2)
     if base == 2:
@@ -74,41 +72,27 @@ def encode(number, base):
         return ''.join(reversed(result))
 
     # TODO: Encode digits to hexadecimal (base 16)
-    if base == 16:
+    elif base == 16:
         while number:
-            result+=number % base
-            print(result)
+            remainder=number % base
+            if remainder < 9:
+                result+=str(str(remainder))
+            else:
+                result+=str(lookup[str(remainder)])
             number = number // base
-        return ''.join(reversed(str(result))
-        # hex code lookup table.
-        # lookup = { '0':'0',  '1':'1',  '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9', '10':'A', '11':'B', '12': 'C', '13':'D', '14':'E', '15':'F', '10':'a', '11':'b', '12':'c', '13':'d', '14':'e', '15':'f'}
-        #
-        # array_numbers = list(str(number)) # '100'
-        # lookup_index = array_numbers.pop() # '0'
-        # test = lookup_index
-        # while array_numbers:
-        #     while test in lookup:
-        #         lookup_index = test
-        #         test += array_numbers.pop()
-        #         print(test, lookup_index, array_numbers)
-        #     else:
-        #         result+=lookup[lookup_index]
-        #         test = test[-1]
-        # else:
-        #     result+=lookup[test[-1]]
-        # return result
+        return ''.join(reversed(str(result)))
 
-
-    # TODO: Encode number in any base (2 up to 36)
-    # else:
-    #     for i, character in enumerate(reversed(number)):
-    #         if lookup[number] < base:
-    #             result += lookup[digits_array.pop()]*base**i
-    #         else:
-    #             raise IndexError()
-    #     return result
-
-print(encode(100,16))
+    # TODO: Encode digits to any base (2 up to 36)
+    #Functio
+    else:
+        while number:
+            remainder=number % base
+            if remainder < 9:
+                result+=str(str(remainder))
+            else:
+                result+=str(lookup[str(remainder)]).upper()
+            number = number // base
+        return ''.join(reversed(str(result)))
 
 
 def convert(digits, base1, base2):
@@ -120,8 +104,21 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
+
+    lookupToHex = { '0':'0',  '1':'1',  '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9', '10':'A', '11':'B', '12': 'C', '13':'D', '14':'E', '15':'F', '10':'a', '11':'b', '12':'c', '13':'d', '14':'e', '15':'f'}
+    lookupFromHex = { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
     # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
+
+    # '101'
+    # if base1 == 2 and base2 == 16:
+        myArray = []
+
+        last = 0
+        for _ in range(0, len(digits)-1, 4):
+            print(last,_,digits[last:_])
+            last = _
+        print(last,len(digits)-1,digits[last:len(digits)])
+
     # TODO: Convert digits from base 2 to base 10 (and vice versa)
     # ...
     # TODO: Convert digits from base 10 to base 16 (and vice versa)
@@ -148,3 +145,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+print(convert('ThisFourPark', 2, 16))
