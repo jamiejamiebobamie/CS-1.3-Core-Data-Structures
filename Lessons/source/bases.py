4a1831ccdfd44f360db3954e9cea364df47807f8
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-01 18:35:41 -0700

    bases.py

--- Lessons/source/bases.py
+++ Lessons/source/bases.py
@@ -17,12 +17,36 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+
+    result = 0
+
+    # an array for popping the least significant bit.
+    digits_array = list(digits)
+
+    # hex code lookup table.
+    lookup = { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
+
     # TODO: Decode digits from binary (base 2)
-    # ...
+    if base == 2:
+        for i, number in enumerate(reversed(digits)):
+            result += int(digits_array.pop()) * base**i
+        return result
+
     # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
+    elif base == 16:
+        for i, number in enumerate(reversed(digits)):
+            result += lookup[digits_array.pop()]*base**i
+        return result
+
+    # TODO: Encode number in any base (2 up to 36)
+    else:
+        for i, number in enumerate(reversed(digits)):
+            if lookup[number] < base:
+                result += lookup[digits_array.pop()]*base**i
+            else:
+                raise IndexError()
+        return result
+
 
 
 def encode(number, base):
@@ -34,12 +58,43 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
+
+    result = ""
+
+    # an array for popping the least significant bit.
+    # digits_array = list(number)
+
+    # hex code lookup table.
+    lookup = { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
+
+    i = 0
+
+    # TODO: Encode digits to binary (base 2)
+    if base == 2:
+        while number:
+            if number % 2:
+                result +='1'
+            else:
+                result+="0"
+            number = number // 2
+        return ''.join(reversed(result))
+
+    # TODO: Encode digits to hexadecimal (base 16)
+    elif base == 16:
+        for i, character in enumerate(reversed(number)):
+            result += lookup[digits_array.pop()]*base**i
+        return result
+
     # TODO: Encode number in any base (2 up to 36)
-    # ...
+    else:
+        for i, character in enumerate(reversed(number)):
+            if lookup[number] < base:
+                result += lookup[digits_array.pop()]*base**i
+            else:
+                raise IndexError()
+        return result
+
+
 
 
 def convert(digits, base1, base2):
