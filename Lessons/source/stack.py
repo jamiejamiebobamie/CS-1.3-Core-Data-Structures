438b7ddd0f9ce93d1a0cabb1fceeddb6508880d5
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-07 15:46:15 -0700

    completed ArrayStack.py having passed all tests

--- Lessons/source/stack.py
+++ Lessons/source/stack.py
@@ -66,13 +66,14 @@ class LinkedStack(object):
 
 # Implement ArrayStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
-# class ArrayStack(object):
+class ArrayStack(object):
 
     def __init__(self, iterable=None):
         """Initialize this stack and push the given items, if any."""
         # Initialize a new list (dynamic array) to store the items
         self.list = list()
         if iterable is not None:
+            print(str(iterable))
             for item in iterable:
                 self.push(item)
 
@@ -82,43 +83,35 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        pass
-        # TODO: Check if empty
+        return not self.list
 
     def length(self):
         """Return the number of items in this stack."""
-        pass
-        # TODO: Count number of items
+        return len(self.list)
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        pass
-        # TODO: Insert given item
+        self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        return self.list
-        # TODO: Return top item, if any
+        return self.list[self.length()-1] if self.length() else None
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
-        pass
-        TODO: Remove and return top item, if any
+        if self.list:
+            return self.list.pop(self.length()-1)
+        else:
+            raise ValueError('Empty list.')
 
 
-Implement LinkedStack and ArrayStack above, then change the assignment below
-to use each of your Stack implementations to verify they each pass all tests
+#Implement LinkedStack and ArrayStack above, then change the assignment below
+#to use each of your Stack implementations to verify they each pass all tests
 
-A = [1,2,3,4,5,6,7,8,9]
-Stack = LinkedStack(A)
-
-print(Stack.is_empty())
-print(Stack.peek())
-print(Stack.length())
 
 # Stack = LinkedStack
 Stack = ArrayStack
