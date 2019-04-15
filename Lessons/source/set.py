7c4f1ea69d36e90c6a8c3ce944c574e02839168a
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-15 10:52:10 -0700

    added set.py class methods

--- Lessons/source/set.py
+++ Lessons/source/set.py
@@ -1,4 +1,5 @@
-# Implement Set class (abstract data type backed by data structure of your choice) with the following set operations as instance methods and properties:
+# Implement Set class (abstract data type backed by data structure of your choice) with the following
+# set operations as instance methods and properties:
 # __init__(elements=None) - initialize a new empty set structure, and add each element if a sequence is given
 # size - property that tracks the number of elements in constant time
 # contains(element) - return a boolean indicating whether element is in this set
@@ -12,6 +13,62 @@
 # Include test cases for each class instance method
 # Annotate all instance methods with complexity analysis of running time and space (memory)
 # Compare the behaviors of your Set class to those of the Python set type and Swift Set type
+class Set(object):
+
+    def __init___(elements=None):
+        self.container = {}
+        self.size = 0
+        for element in elements:
+            self.add(element)
+
+
+    def contains(element):
+        if element in self.container:
+            return True
+
+    def add(element):
+        if not self.contains(element):
+            self.container[element] = 1
+            self.size += 1
+
+
+    def remove(element):
+        if self.contains(element):
+            self.container.pop('element', None)
+            self.size -= 1
+        else:
+            raise KeyError("Element not in set.")
+
+    def union(other_set):
+
+        new_set = Set()
+
+
+
+
+    def intersection(other_set):
+    """Iterate through the smaller set and the items contained
+            in both sets to a new set."""
+
+        new_set = Set()
+
+        if self.size > other_set.size:
+            for element in other_set.container:
+                if self.contains(element):
+                    new_set.add(element)
+        else:
+            for element in self.container:
+                if other_set.contains(element):
+                    new_set.add(element)
+
+        return new_set
+
+        - return a new set that is the intersection of this set and other_set
+    def difference(other_set) - return a new set that is the difference of this set and other_set
+    def is_subset(other_set) - return a boolean indicating whether other_set is a subset of this set
+
+
+
 # Stretch Challenges
 #
 # Implement CircularBuffer class (backed by dynamic array) with the following instance methods and properties:
