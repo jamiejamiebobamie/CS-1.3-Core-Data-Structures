4f1112b7a099e7f5d4917744c8a3bb9c8639d0b8
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-15 11:22:13 -0700

    finished set.py file

--- Lessons/source/set.py
+++ Lessons/source/set.py
@@ -1,14 +1,3 @@
-# Implement Set class (abstract data type backed by data structure of your choice) with the following
-# set operations as instance methods and properties:
-# __init__(elements=None) - initialize a new empty set structure, and add each element if a sequence is given
-# size - property that tracks the number of elements in constant time
-# contains(element) - return a boolean indicating whether element is in this set
-# add(element) - add element to this set, if not present already
-# remove(element) - remove element from this set, if present, or else raise KeyError
-# union(other_set) - return a new set that is the union of this set and other_set
-# intersection(other_set) - return a new set that is the intersection of this set and other_set
-# difference(other_set) - return a new set that is the difference of this set and other_set
-# is_subset(other_set) - return a boolean indicating whether other_set is a subset of this set
 # Write unit tests to ensure the Set class is robust
 # Include test cases for each class instance method
 # Annotate all instance methods with complexity analysis of running time and space (memory)
@@ -21,7 +10,6 @@ class Set(object):
         for element in elements:
             self.add(element)
 
-
     def contains(element):
         if element in self.container:
             return True
@@ -31,7 +19,6 @@ class Set(object):
             self.container[element] = 1
             self.size += 1
 
-
     def remove(element):
         if self.contains(element):
             self.container.pop('element', None)
@@ -40,15 +27,33 @@ class Set(object):
             raise KeyError("Element not in set.")
 
     def union(other_set):
+        """Iterate over two sets and create a new set from the elemets contained in both.
+        This method is meant to decrease overall time complexity by iterating
+        over an element once. The space complexity is high as the other_set is duplicated
+        in order to pop elements from it.
+
+        TO-DO: Perhaps write an if-statement that begins the function that causes the program
+        to follow a different set of instructions if the size of the other_set exceeds a given
+        amount."""
 
         new_set = Set()
 
+        other_set_copy = other_set
 
+        for element in self.container:
+
+            if other_set_copy.contains(element):
+                other_set_copy.remove(element)
+
+            new_set.add(element)
+
+        for element in other_set_copy.container:
+            new_set.add(element)
 
 
     def intersection(other_set):
-    """Iterate through the smaller set and the items contained
-            in both sets to a new set."""
+    """Iterate through the smaller set and look for items contained
+            in both sets and add them to a new set."""
 
         new_set = Set()
 
@@ -63,11 +68,40 @@ class Set(object):
 
         return new_set
 
-        - return a new set that is the intersection of this set and other_set
-    def difference(other_set) - return a new set that is the difference of this set and other_set
-    def is_subset(other_set) - return a boolean indicating whether other_set is a subset of this set
 
+    def difference(other_set):
+        """Iterate over the two sets and find the elements
+        that are not shared and add them to a new set.
+        Again trying to decrease overall time complexity by copying the set,
+        but the trade off might not be worth it."""
 
+        new_set = Set()
+
+        other_set_copy = other_set
+
+        for element in this.container:
+            if not other_set_copy.contains(element):
+                new_set.add(element)
+            else:
+                other_set_copy.remove(element)
+
+        for element in other_set_copy.container:
+            if not self.contains(element):
+                new_set.add(element)
+
+    def is_subset(other_set):
+        """Iterate over the other set and check to see if each element is contained
+        in this set. Return False if an element is not found.
+        Return True if the entire set has been iterated over.
+        Don't iterate over anything if the size of the other_set is larger than this set."""
+
+        if self.size > other_set.size:
+            for element in other_set.container:
+                if not self.contains(element)
+                    return False
+            return True
+        else:
+            return False
 
 # Stretch Challenges
 #
