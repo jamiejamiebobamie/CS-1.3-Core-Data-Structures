9109a2744f69462e9faf6a1906a194af1c6e34a9
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-15 20:48:03 -0700

    added docstrings and time complexity ananlysis.

--- project/CallRoutingProject.py
+++ project/CallRoutingProject.py
@@ -4,7 +4,9 @@ routes = '/Users/jamesmccrory/documents/dev/CS-1.3-Core-Data-Structures/project/
 numbers = '/Users/jamesmccrory/documents/dev/CS-1.3-Core-Data-Structures/project/data/phone-numbers-3.txt'
 
 def createRouteCostDictionary(filePath):
-
+    """Time complexity: O(2n). Space complexity O(2n).
+    Read the file and split the lines into an array.
+    Read the array and return a dictionary of routes to costs."""
     f = open(str(filePath))
     lines = f.read().split()
     f.close()
@@ -16,6 +18,8 @@ def createRouteCostDictionary(filePath):
     return dictionary
 
 def readPhoneNumbers(filePath):
+    """Time complexity: O(n). Space complexity O(n).
+    Read the file and split the lines into an array. Return the array."""
     f = open(str(filePath))
     numbersArray = f.read().split()
     f.close()
@@ -23,6 +27,19 @@ def readPhoneNumbers(filePath):
     return numbersArray
 
 def findLowestCost(routes, phoneNumbers):
+    """Time complexity: O(n**3). Space complexity O(2n).
+
+    Iterate through each phone number in the array of phone numbers,
+    for each number grow a 'test' string by adding each digit.
+    Test if that 'test' string is a key in the dictionary.
+    If it is, check to see if it is the lowest cost for that number.
+    If the 'test' string is not in the dictionary then, move onto
+    the next phone number after adding that phone number and it's lowest cost to
+    the output file.
+
+    A string concatenation inside a nested for loop makes this function have
+    an 'n-cubed' time complexity."""
+
     file = ""
     for phoneNumber in phoneNumbers:
         test = ""
@@ -31,6 +48,8 @@ def findLowestCost(routes, phoneNumbers):
             test += digit
             if test in routes:
                 minimum = min(minimum,float(routes[test]))
+            else:
+                continue
         if minimum != float('inf'):
             file += str(phoneNumber)+','+ str(minimum)+'\n'
         else:
