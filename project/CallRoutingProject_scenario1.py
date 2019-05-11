ccfef8c17317bda9a2ba2ceb5cf11ea9a2096a62
jamiejamiebobamie <jmccrory@vt.edu>
2019-05-10 19:33:43 -0700

    refactored solutions 1 and 2

--- project/CallRoutingProject_scenario1.py
+++ project/CallRoutingProject_scenario1.py
@@ -1,85 +1,44 @@
 
-#SCENARIO #1
-
 """
+SCENARIO #1
+
 As there is no order to the routes in the route file, the entirety of the file
-has to be read. Because each route has a cost associated with it, it makes sense
-to create a dictionary of routes to costs, key-value pairs.
+has to be read.
 
-But having only one number to check seems to elimate a large portion
-of the possible routes. So it seems wasteful and more work than is necessary
-to create an entire dictionary...
+Open the file. Iterate through it. Searching each digit of each route.
+If the program reaches the end of a route, we check to see if that route is cheaper than the current
+lowest price and change the lowest to that route's cost if it is.
 
-If there was some way to iterate through the file and check
-line by line for subsets of the phone number in the routes,
-the time complexity would still be O(n**2), but the check would require
-one function and be more succinct.
+Finally returning the lowestRouteCost when all routes in the route file have been read.
+
+Call the function in the terminal with the desired phone number to lookup.
+This function assumes their is a fixed route file list that is hardcoded into the function.
+
 """
+
+import sys
 
 routes = '/Users/jamesmccrory/documents/dev/CS-1.3-Core-Data-Structures/project/data/route-costs-4.txt'
 numbers = '/Users/jamesmccrory/documents/dev/CS-1.3-Core-Data-Structures/project/data/phone-numbers-3.txt'
 
-def createRouteCostDictionary(filePath):
-    """Time complexity: O(2n+m), Space complexity O(2n+m),
-    n the length of all the characters in the file,
-    and m the number of lines in the file (which become array elements).
+def iterateThroughRoutesFile(routesFile, phoneNumber):
+    """Time complexity: O(n**2), n being the number of routes and the length of their digits.
+        Space complexity: O(1), the lowestRouteCost variable."""
+    lowestRouteCost = float('inf')
 
-    Read the file and split the lines into an array.
-    Read the array and return a dictionary of routes to costs.
-
-    TO-DO: Create the dictionary with one pass over the file."""
-
-    f = open(str(filePath))
-    lines = f.read().split()
-    f.close()
-    dictionary = {}
-    for line in lines:
-        route, cost=line.split(",")
-        if dictionary.get(route, None): # check to see if the entry exists in the dictionary
-            if dictionary[route] > cost:
-                dictionary[route] = cost
+    for line in open(routesFile):
+        i = 0 # index
+        route, cost=line.split(",")# O(n)
+        while route[i] == phoneNumber[i] and i < len(route) - 1:
+            i += 1
         else:
-            dictionary[route] = cost
+            if i == len(route) - 1:# O(1)
+                cost = float(cost.strip("\n")) #O(n)
+                if lowestRouteCost > cost:
+                    lowestRouteCost = cost
+    return lowestRouteCost if lowestRouteCost != float('inf') else "Your route does not exist."
 
-    return dictionary
 
-def readPhoneNumber(filePath):
-    """Time complexity: O(n). Space complexity O(n),
-    the length of the phone number.
-
-    Read the first line of the file and return the phone number."""
-    f = open(str(filePath))
-    phoneNumber = f.readline().strip('\n')
-    f.close()
-
-    return phoneNumber
-
-def findLowestCost(routes, phoneNumber):
-    """Time complexity: O(n**2). Space complexity O(2n).
-
-    Iterate through the digits of the phone number, growing a 'test' string
-    by adding each digit. Test if that 'test' string is a key in the dictionary.
-
-    If it is, check to see if it is the lowest cost for that number.
-
-    If the 'test' string is not in the dictionary then return the result.
-
-    As there is only one string to iterate through while concatenating a 'test'
-    string-key this function has an O(n**2) time complexity."""
-
-    test = ""
-    minimum = float('inf')
-    result = ""
-
-    for digit in phoneNumber:
-        test = test.join(digit)
-        if test in routes:
-            minimum = min(minimum,float(routes[test]))
-        else:
-            if minimum != float('inf'):
-                result = str(phoneNumber)+','+ str(minimum)
-            else:
-                result = str(phoneNumber)+','+ str(0)
-    return result
-
-print(findLowestCost(createRouteCostDictionary(routes), readPhoneNumber(numbers)))
+if __name__ == "__main__":
+    phoneNumber = str(sys.argv[1])
+    print(iterateThroughRoutesFile(routes, phoneNumber))
--- project/CallRoutingProject_scenario2.py
+++ project/CallRoutingProject_scenario2.py
@@ -1,30 +1,27 @@
 
 #SCENARIO #2
 
-import timeit
-
-
 """
 
-Similar solution to solution #1, but the functions find the lowest costs for all
-phone numbers in the file.
+
 
 """
 
 routes = '/Users/jamesmccrory/documents/dev/CS-1.3-Core-Data-Structures/project/data/route-costs-10000000.txt'
 numbers = '/Users/jamesmccrory/documents/dev/CS-1.3-Core-Data-Structures/project/data/phone-numbers-1000.txt'
 
-def createRouteCostDictionary(filePath):
-    """Time complexity: O(2n). Space complexity O(2n).
-    Read the file and split the lines into an array.
+def createRouteCostDictionary(routesFile):
+    """Time complexity: O(2n*m), Space complexity O(2n+m).
+    n, the number of lines in the file.
+    m, the number of characters in the line.
+
+    Read the file and split the lines into an array of two elements: [route,cost].
     Read the array and return a dictionary of routes to costs."""
-    f = open(str(filePath))
-    lines = f.read().split()
-    f.close()
+
     dictionary = {}
-    for line in lines:
+    for line in open(routesFile):
         route, cost=line.split(",")
-        if dictionary.get(route, None): # check to see if the entry exists in the dictionary
+        if route in dictionary:
             if dictionary[route] > cost:
                 dictionary[route] = cost
         else:
@@ -35,47 +32,39 @@ def createRouteCostDictionary(filePath):
 def readPhoneNumbers(filePath):
     """Time complexity: O(n). Space complexity O(n).
     Read the file and split the lines into an array. Return the array."""
+
     f = open(str(filePath))
     numbersArray = f.read().split()
     f.close()
-
     return numbersArray
 
 def findLowestCost(routes, phoneNumbers):
-    """Time complexity: O(n**3). Space complexity O(2n).
+    """Time complexity: O(n**2). Space complexity O(2n).
 
-    Iterate through each phone number in the array of phone numbers,
-    for each number grow a 'test' string by adding each digit.
-    Test if that 'test' string is a key in the dictionary.
+    Iterate through the digits of the phone number, growing a 'test' string
+    by adding each digit. Test if that 'test' string is a key in the dictionary.
+
     If it is, check to see if it is the lowest cost for that number.
-    If the 'test' string is not in the dictionary then, move onto
-    the next phone number after adding that phone number and it's lowest cost to
-    the output file.
 
-    A string concatenation inside a nested for loop makes this function have
-    an 'n-cubed' time complexity."""
+    If the 'test' string is not in the dictionary then return the result.
 
-    file = ""
-    for phoneNumber in phoneNumbers:
-        test = ""
-        minimum = float('inf')
+    As there is only one string to iterate through while concatenating a 'test'
+    string-key, this function has an O(n**2) time complexity."""
+
+    test = ""
+    minimum = float('inf')
+    results = []
+    print(len(phoneNumbers))
+    for phoneNumber in phoneNumbers: # O(n)
         for digit in phoneNumber:
-            test += digit
+            test += digit # O(n) <-- nested
             if test in routes:
                 minimum = min(minimum,float(routes[test]))
-        if minimum != float('inf'):
-            file += str(phoneNumber)+','+ str(minimum)+'\n'
-        else:
-            file += str(phoneNumber)+','+ str(0)+'\n'
-
-    return file
+            else:
+                if minimum != float('inf'):
+                    results.append((phoneNumber, str(minimum)))
+                else:
+                    results.append((phoneNumber, "Your route does not exist."))
+    return results
 
 print(findLowestCost(createRouteCostDictionary(routes), readPhoneNumbers(numbers)))
-
-
-
-# t = Timer()       # outside the try/except
-# try:
-#     t.timeit(...)    # or t.repeat(...)
-# except:
-#     t.print_exc()
